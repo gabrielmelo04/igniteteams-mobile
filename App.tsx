c6ac21bbc9e23dad62dea119acc6af3ef321e647
gabrielmelo04 <gabriel_m123@hotmail.com>
2025-03-31 12:26:13 -0300

    new: add fonts and component Header and component Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,32 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
 import { Groups } from './src/screens/Groups';
+import theme from './src/theme';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        fontsLoaded ?
+          <Groups />
+          :
+          <Loading />
+      }
+    </ThemeProvider>
   );
 }
--- src/styled-components.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'styled-components/native' {
-  import * as styled from 'styled-components/native';
-  export default styled;
-}
